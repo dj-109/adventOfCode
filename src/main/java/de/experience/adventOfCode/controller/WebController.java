31bb5ada898855ba2d1c870afde1ab26fc2a3957
DJE <dennis.jeschke@gmail.com>
2020-09-30 22:43:03 +0200

    input page

--- src/main/java/de/experience/adventOfCode/controller/WebController.java
+++ src/main/java/de/experience/adventOfCode/controller/WebController.java
@@ -27,4 +27,5 @@ public class WebController {
         return ban + "babanana";
     }
 
+
 }
---
+++ src/main/java/de/experience/adventOfCode/model/Input.java
@@ -0,0 +1,23 @@
+package de.experience.adventOfCode.model;
+
+public class Input {
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private long id;
+    private String content;
+
+}
