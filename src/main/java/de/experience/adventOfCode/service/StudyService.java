7d44be001d4a3552309309c45c5b4bb5050a6292
exde365b <dennis.jeschke.ext@kiongroup.com>
2020-10-02 16:03:17 +0200

    add application.properties thymeleaf cache configuration

--- src/main/java/de/experience/adventOfCode/service/StudyService.java
+++ src/main/java/de/experience/adventOfCode/service/StudyService.java
@@ -1,10 +1,10 @@
-package de.experience.adventOfCode;
+package de.experience.adventOfCode.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component
+
 public class StudyService {
 
     public String ausprobieren(String input){
